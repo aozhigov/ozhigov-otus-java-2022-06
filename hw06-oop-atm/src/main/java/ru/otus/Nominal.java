f8da08135bc4947e6a4eb3d9deaa9e4228c6a6f4
andrey0811 <0811andrey0811@mail.ru>
2022-09-23 08:32:09 +0500

    hw6

---
+++ hw06-oop-atm/src/main/java/ru/otus/Nominal.java
@@ -0,0 +1,40 @@
+package ru.otus;
+
+import java.util.Arrays;
+
+/**
+ *
+ *
+ * @author aozhigov
+ * @since 9/22/22
+ */
+public enum Nominal {
+
+    FIFTY(50),
+
+    ONE_HUNDRED(100),
+
+    FIVE_HUNDRED(500),
+
+    THOUSAND(1000),
+
+    TWO_THOUSAND(2000),
+
+    FIVE_THOUSAND(5000);
+
+    public static Nominal of(int value) {
+        return Arrays.stream(Nominal.values())
+                .filter(it -> it.value == value)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Нет подходящего номинала: " + value));
+    }
+    private final int value;
+
+    Nominal(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
